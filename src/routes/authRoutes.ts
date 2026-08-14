b9d757664acfac2d7c742e6e7ecc9ef6bbeca013
Argi <argiigmg@gmail.com>
2026-08-14 18:33:10 +0700

    feat: update auth routes to include '/auth/'

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,10 +4,11 @@ import { authentication } from '../middlewares/authentication.middleware.js';
 
 const router = Router();
 
-router.post('/register', Register);
-router.post('/login', Login);
-router.post('/refresh', Refresh);
-router.post('/logout', Logout);
+router.post('/auth/register', Register);
+router.post('/auth/login', Login);
+router.post('/auth/refresh', Refresh);
+router.post('/auth/logout', Logout);
+
 router.get('/me', authentication, Me);
 
 export default router;
